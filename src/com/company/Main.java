f99df39c987966182582f3404903ca2af2c8c3ea
Maxim Kasyanov <kasyanov.maxim@outlook.com>
2019-01-29 16:04:44 +0600

    some change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(222);
+    System.out.println("Main.main");
+
+
     System.out.println(333);
   }
 }
