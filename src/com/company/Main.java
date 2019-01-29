d368e8bb57ae80eae908e6e2c2e0de57dd828930
Maxim Kasyanov <kasyanov.maxim@outlook.com>
2019-01-29 16:03:29 +0600

    Change sout in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(111);
+    System.out.println(222);
+    System.out.println(333);
   }
 }
